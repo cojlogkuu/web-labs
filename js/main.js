985e3150cc644b0d294e83f16d537a25067b8a9d
cojlogkuu <babiradrl@gmail.com>
2024-09-18 10:14:59 +0300

    add sorting by type and total price counter

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ import { getPetsValues, clearInputs, addItem, renderAllItems } from "./dom_utils
 const submitButton = document.getElementById('submit_button');
 const findButton = document.getElementById('find_button');
 const textFindInput = document.getElementById('text_find');
+const typeSelectFind = document.getElementById('type_find');
 const cancelButton = document.getElementById('cancel_button');
+const countButton = document.getElementById('count_button');
 
 let petItems = [];
 
@@ -17,16 +19,39 @@ submitButton.addEventListener('click', (event) => {
     clearInputs();
 });
 
-findButton.addEventListener('click', () => {
-    const filtredPets = petItems.filter(pet => 
+let filtredPets = [];
+
+function filterPets () {
+    if (typeSelectFind.value !== 'all') {
+        filtredPets = petItems.filter(pet => pet.type === typeSelectFind.value)
+    } else {
+        filtredPets = petItems;
+    };
+
+
+    filtredPets = filtredPets.filter(pet => 
         pet.name.search(textFindInput.value) !== -1);
 
     renderAllItems(filtredPets);
+}
+
+findButton.addEventListener('click', () => {
+    filterPets();
 });
 
 cancelButton.addEventListener('click', () => {
     textFindInput.value = '';
+    typeSelectFind.value = 'all';
+    filtredPets = [];
     renderAllItems(petItems);
 });
+
+countButton.addEventListener('click', () => {
+    filterPets();
+    let totalPrice = 0;
+    filtredPets.forEach(pet => totalPrice += +pet.price);
+    alert(`Total price of pets is ${totalPrice}$`);
+})
+
 
 
